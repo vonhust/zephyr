8054dc8dc4be6b1471d7a1ae4b16fb18b4f34cbc
Wayne Ren <renwei41@huawei.com>
2022-06-24 20:40:10 +0800

    intc_gicv3: modification for OpenAMP support
    Just like SMP initialization, when OpenAMP is enabled,
    the OpenAMP client can't init the global part of GIC which is
    done by OpenAMP host.
    
    Signed-off-by: Wayne Ren <renwei41@huawei.com>

--- drivers/interrupt_controller/intc_gicv3.c
+++ drivers/interrupt_controller/intc_gicv3.c
@@ -588,7 +588,10 @@ static void __arm_gic_init(void)
 int arm_gic_init(void)
 {
 
-	gicv3_dist_init();
+/* openamp client does not init the global part of GIC */
+	if (!IS_ENABLED(CONFIG_OPENAMP_SLAVE)) {
+		gicv3_dist_init();
+	}
 
 	__arm_gic_init();
 
