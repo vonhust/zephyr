9c4e65b35d5c7a1e4f95cb05a750d172f6ce4f25
Wayne Ren <renwei41@huawei.com>
2024-05-19 11:16:33 +0800

    rpi_4b: add openocd debug support
    use jlink as debugger
    
    Signed-off-by: Wayne Ren <renwei41@huawei.com>

--- boards/arm64/rpi_4b/board.cmake
+++ boards/arm64/rpi_4b/board.cmake
@@ -1,1 +1,5 @@
+# Copyright (c) 2024 openEuler Embedded
 # SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(openocd "--use-elf" "--config=${BOARD_DIR}/support/rpi_4b.cfg")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
